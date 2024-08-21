ab9c99a575906c149cef0e15bf9599cc517a76a0
redig <luizredig@gmail.com>
2024-08-21 00:54:11 -0300

    feat: add new Motorcycle class

---
+++ src/com/github/luizredig/java/vehicles/model/classes/concretes/vehicles/Motorcycle.java
@@ -0,0 +1,6 @@
+package com.github.luizredig.java.vehicles.model.classes.concretes.vehicles;
+
+import com.github.luizredig.java.vehicles.model.classes.abstracts.Vehicle;
+
+public class Motorcycle extends Vehicle {
+}
