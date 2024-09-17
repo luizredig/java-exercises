543159bcb282e3da1f8c849b591c22710d5439fe
redig <luizredig@gmail.com>
2024-09-17 20:26:55 -0300

    feat(factory): add NotificationFactory abstract class

---
+++ src/com/github/luizredig/java/factoryMethod/model/abstracts/NotificationFactory.java
@@ -0,0 +1,7 @@
+package com.github.luizredig.java.factoryMethod.model.abstracts;
+
+import com.github.luizredig.java.factoryMethod.model.interfaces.INotification;
+
+public abstract class NotificationFactory {
+    public abstract INotification createNotification();
+}
